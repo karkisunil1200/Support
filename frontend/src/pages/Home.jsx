770d89d629e3a69d52265540edd3a1e7178d9220
sunilkarki <karkisunil1200@gmail.com>
2022-02-24 14:17:30 -0600

    Added login and register

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { FaQuestionCircle, FaTicketAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <>
+      <section className="heading">
+        <h2>What do you need elp with? </h2>
+        <p>Please choose from an option below</p>
+      </section>
+
+      <Link to="/new-ticket" className="btn btn-reverse btn-block">
+        <FaQuestionCircle />
+        Create New Ticket
+      </Link>
+
+      <Link to="/ticket" className="btn btn-block">
+        <FaTicketAlt />
+        View My Ticket
+      </Link>
+    </>
+  );
+};
+
+export default Home;
